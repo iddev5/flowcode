55a29fa714245ee2685d239a39e7885a79b603d2
iddev5 <ayushbardhan5@gmail.com>
2026-03-08 23:07:18 +0530

    frontend: Fix formatting

--- frontend-tw/src/Home.tsx
+++ frontend-tw/src/Home.tsx
@@ -1,22 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import api from "@/lib/api";
-import { useAuth } from '@/components/auth-provider';
-import { useNavigate } from 'react-router';
-import { Plus } from 'lucide-react';
-import { setCurrentProject } from '@/features/projectSlice';
-import { ProfileTrigger } from '@/sections/Header'
+import { useAuth } from "@/components/auth-provider";
+import { useNavigate } from "react-router";
+import { Plus } from "lucide-react";
+import { setCurrentProject } from "@/features/projectSlice";
+import { ProfileTrigger } from "@/sections/Header";
 import { useAppDispatch, useAppSelector, type RootState } from "./lib/store";
 
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const { user, loading } = useAuth();
   const dispatch = useAppDispatch();
-  const projects = useAppSelector((state: RootState) => state.projects.projects);
+  const projects = useAppSelector(
+    (state: RootState) => state.projects.projects,
+  );
   const navigate = useNavigate();
 
-  if (!loading && user === null)
-    navigate('/');
+  if (!loading && user === null) navigate("/");
 
   useEffect(() => {
     const T = async () => {
@@ -26,37 +27,41 @@ export default function Home() {
       }
     };
     T();
-  }, [loading])
+  }, [loading]);
 
   const newProject = async () => {
-    navigate('/project/new');
-  }
+    navigate("/project/new");
+  };
 
   const setProject = (proj) => {
     navigate(`/project/${proj._id}`);
-  }
+  };
 
   return (
     <>
-    <div className="w-screen flex flex-col justify-center items-center">
-      <div className="w-[40vw] flex items-center gap-5 mt-5 pb-5 border-b-1 border-black/10">
-        <ProfileTrigger />
-        <h1>{user?.displayName}</h1>
-      </div>
-      <div className="w-[40vw] m-8 flex flex-col">
-        <Button className="w-min" onClick={newProject}>
-          New Project
-          <Plus />
-        </Button>
-        <div className="mt-4 flex flex-col gap-2 items-start">{
-            projects.map(proj =>
-              <Button className="w-full justify-start" variant="secondary" onClick={() => setProject(proj)}>
+      <div className="w-screen flex flex-col justify-center items-center">
+        <div className="w-[40vw] flex items-center gap-5 mt-5 pb-5 border-b-1 border-black/10">
+          <ProfileTrigger />
+          <h1>{user?.displayName}</h1>
+        </div>
+        <div className="w-[40vw] m-8 flex flex-col">
+          <Button className="w-min" onClick={newProject}>
+            New Project
+            <Plus />
+          </Button>
+          <div className="mt-4 flex flex-col gap-2 items-start">
+            {projects.map((proj) => (
+              <Button
+                className="w-full justify-start"
+                variant="secondary"
+                onClick={() => setProject(proj)}
+              >
                 <p className="truncate">{proj.name}</p>
               </Button>
-            )
-        }</div>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 }
