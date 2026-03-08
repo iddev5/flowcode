175b2b2298c29e98b8368b712d48b7451fe197ea
iddev5 <ayushbardhan5@gmail.com>
2026-03-08 23:06:02 +0530

    frontend: Dont show sign in button if loading

--- frontend-tw/src/Index.tsx
+++ frontend-tw/src/Index.tsx
@@ -4,31 +4,33 @@ import { getAdditionalUserInfo, signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "./lib/firebase";
 import { useAppDispatch } from "./lib/store";
 import api from "./lib/api";
-import { useAuth } from '@/components/auth-provider';
+import { useAuth } from "@/components/auth-provider";
 import { useNavigate } from "react-router";
 
 export default function Index() {
-    const {user, loading} = useAuth();
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
+  const { user, loading } = useAuth();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-    if (!loading && user !== null)
-      navigate('/home');
+  if (!loading && user !== null) navigate("/home");
 
-    const signIn = async () => {
-        const res = await signInWithPopup(auth, googleProvider);
-        const additionalInfo = getAdditionalUserInfo(res);
+  const signIn = async () => {
+    const res = await signInWithPopup(auth, googleProvider);
+    const additionalInfo = getAdditionalUserInfo(res);
 
-        dispatch(api.createUser());
-        navigate('/home');
-    }
+    dispatch(api.createUser());
+    navigate("/home");
+  };
 
-    return (
-      <div className="w-screen h-screen flex flex-col gap-10 items-center justify-center">
-        <h1 className="font-bold text-9xl">Flowcode</h1>
+  return (
+    <div className="w-screen h-screen flex flex-col gap-10 items-center justify-center">
+      <h1 className="font-bold text-9xl">Flowcode</h1>
+      {loading && <p>Loading. Please wait...</p>}
+      {!loading && (
         <Button onClick={signIn}>
-            <Mail /> Sign in with Google
+          <Mail /> Sign in with Google
         </Button>
-      </div>
-    );
+      )}
+    </div>
+  );
 }
